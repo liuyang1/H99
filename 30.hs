b0661331f7cc2c38ef5e33ebbc015c90d86432c7
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-04-07 23:59:00 +0800

    34..41

--- 30.hs
+++ 30.hs
@@ -18,7 +18,39 @@ gcd' x y
 coprime x y = 1 == gcd' x y
 
 -- 34
-totient n = sum $ map (\ x -> if isPrime x then 1 else 0) [2..n]
+totient n = length $ filter (coprime n) [2..(n+1)]
 
 -- 35
--- findFactor b v = if 
+findfactor v (x: xs)
+    | v `mod` x == 0 = x
+    | otherwise = findfactor v xs
+primefactors 1 = []
+primefactors v =
+    let f0 = findfactor v (filter isPrime [2..(v+1)])
+       in f0: primefactors (v `div` f0)
+
+-- 36
+encodeI x n [] = ((x, n), [])
+encodeI x n (y: xs)
+  | x == y = encodeI x (n + 1) xs
+  | otherwise = ((x, n), y: xs)
+encode [] = []
+encode (x: xs) = let (f, s) = encodeI x 1 xs in f: encode s
+primefactorsM = encode . primefactors
+
+-- 37
+phi mm = product $ map (\(p, m) -> (p - 1) * p ^ (m - 1)) $ primefactorsM mm
+
+-- 38
+-- speed test: 34 vs. 37
+
+-- 39
+primeR b e = filter isPrime [b..e]
+
+-- 40
+-- need e greater than 2
+goldbach e = head $ filter (\(a, b) -> isPrime a && isPrime b) $ map (\x -> (x, e - x)) [2..(e `div` 2)]
+
+-- 41
+goldbachl b e = map goldbach $ filter even [b..e]
+goldbachl' b e thresh = filter (\(x, _) -> x > thresh) $ goldbachl b e
