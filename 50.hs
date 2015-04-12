327dd16faa44bd896509d1e2f10017b8c6a9356f
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-04-12 22:19:58 +0800

    56..59

--- 50.hs
+++ 50.hs
@@ -10,3 +10,33 @@ cbalTree n
                  ts1 = cbalTree (n `div` 2 - 1)
               in [Branch "x" t0 t1 | t0 <- ts0, t1 <-ts1]
               ++ [Branch "x" t1 t0 | t0 <- ts0, t1 <-ts1]
+
+-- 56
+-- symmetric
+mirror Empty = Empty
+mirror (Branch a t0 t1) = Branch a (mirror t1) (mirror t0)
+samest Empty Empty = True
+samest (Branch a a0 a1) (Branch b b0 b1) = samest a0 b0 && samest a1 b1
+samest t0 t1 = False
+symmetric t = samest t $ mirror t
+
+-- 57
+construct [] = Empty
+construct (x0: xs) = let t0 = construct [x | x <- xs, x <= x0]
+                         t1 = construct [x | x <- xs, x > x0]
+                      in Branch x0 t0 t1
+
+-- 58
+symCbalTrees n = [x | x <- cbalTree n, symmetric x]
+
+-- 59
+hbalTree c 0 = [Empty]
+hbalTree c 1 = [Branch c Empty Empty]
+hbalTree c n = let tn1 = hbalTree c (n - 1)
+                   tn2 = hbalTree c (n - 2)
+                in [Branch c t1 t2 | t1 <- tn1, t2 <- tn2]
+                ++ [Branch c t1 t2 | t1 <- tn2, t2 <- tn1]
+                ++ [Branch c t1 t2 | t1 <- tn2, t2 <- tn2]
+
+-- 60
+-- TODO
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # H99
-haskell 99 solution
+Haskell 99 solution
+
+## TODO
+- 23..25        random
+- 50            huffman
+- 59            hbal tree node
+
+## TBD
+- 61..69        binary tree
+- 70..73        multiway tree
+- 80..89        graph
+- 90..94        miscellaneous problems
+- 95..99        miscellaneous problems, again
+
+## DONE
+- 1..28         list
+- 31..41        arithmetic
+- 46..50        logic and code
+- 54..59        binary tree
